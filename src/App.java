e35e422fea75a9a940b83c9d0cbca23e91bcd8b8
houssam <daoudi.houssam.03@gmail.com>
2024-05-18 10:45:38 +0100

    last commit finish game coding

--- src/App.java
+++ src/App.java
@@ -9,10 +9,11 @@ public class App {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        
+
         FlappyBird flappyBird = new FlappyBird();
         frame.add(flappyBird);
         frame.pack();
+        flappyBird.requestFocus();
         frame.setVisible(true);
 
     }
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener, KeyListener{
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -13,7 +13,51 @@ public class FlappyBird extends JPanel {
     Image topPipeImg;
     Image bottomPipeImg;
 
-    //Bird
+    // Game Logic
+    Bird bird;
+    Timer gameLoop;
+    int velocityX = -4;
+    int velocityY=0;
+    int gravity = 1;
+    Pipe pipe;
+    ArrayList<Pipe> pipes;
+    Random random = new Random();
+    Timer placePipesTimer;
+
+    boolean gameOver = false;
+    double score = 0;
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+
+        setFocusable(true);
+        addKeyListener(this);
+        // load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        
+        //bird
+        bird = new Bird(birdImg);
+        //pipes
+        pipes = new ArrayList<Pipe>();
+
+        //game timer
+        gameLoop = new Timer(1000 / 60, this);
+        gameLoop.start();
+
+        //Place Pipes loop 
+        placePipesTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+        placePipesTimer.start();
+    }
+
+    // Bird
     int birdX = boardWidth / 8;
     int birdY = boardHeight / 2;
     int birdWidth = 34;
@@ -31,15 +75,34 @@ public class FlappyBird extends JPanel {
         }
     }
 
-    FlappyBird() {
-        setPreferredSize(new Dimension(boardWidth, boardHeight));
+    //pipes
+    int pipesX = boardWidth;
+    int pipesY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
 
-        // load images
-        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
-        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
-        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
-        Bird bird = new Bird(birdImg);
+    class Pipe {
+        int x = pipesX;
+        int y = pipesY;
+        int width = pipeWidth;
+        int height = pipeHeight;
+        Image img;
+        boolean passed;
+
+        Pipe(Image img) {
+            this.img = img;
+        }
+    }
+
+    public void placePipes() {
+        int randomPipeY = (int) (pipesY - pipeHeight / 4 - Math.random() * (pipeHeight / 2));
+        int openingSpace = boardHeight / 4;
+        Pipe topPipe = new Pipe(topPipeImg);
+        topPipe.y = randomPipeY;
+        pipes.add(topPipe);
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
+        pipes.add(bottomPipe);
     }
 
     public void paintComponent(Graphics g) {
@@ -48,7 +111,92 @@ public class FlappyBird extends JPanel {
     }
 
     public void draw(Graphics g) {
+        // background
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+
+        //bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+        //pipes
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+        if (gameOver) {
+            g.drawString("Game Over: "+String.valueOf((int) score),10,35);
+        } else {
+            g.drawString(String.valueOf((int) score), 10, 35);
+        }
+
     }
-    
+
+    public void move() {
+        //bird
+        velocityY += gravity;
+        bird.y += velocityY;
+        bird.y = Math.max(bird.y, 0);
+        //pipes
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX;
+
+            if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+                pipe.passed = true;
+                score += 0.5;
+            }
+
+            if (collision(bird, pipe)) {
+                gameOver = true;
+            }
+        }
+        if (bird.y > boardHeight) {
+            gameOver = true;
+        }
+    }
+
+    public boolean collision(Bird a, Pipe b) {
+        return a.x<b.x+b.width&&
+        a.x+a.width>b.x&&
+        a.y<b.y+b.height&&
+        a.y+a.height>b.y;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+        if (gameOver) {
+            placePipesTimer.stop();
+            gameLoop.stop();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            velocityY = -9;
+        }
+        if (gameOver) {
+            bird.y = birdY;
+            velocityY = 0;
+            pipes.clear();
+            score = 0;
+            gameOver = false;
+            gameLoop.start();
+            placePipesTimer.start();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+     }
+
 }
